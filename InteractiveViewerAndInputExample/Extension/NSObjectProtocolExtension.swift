0c89450e4d2b1a53c9f4dc8af346ab39695f6e09
Fumiya Sakai <just1factory@gmail.com>
2020-12-28 22:25:26 +0900

    [WIP] ModuleやExtensionの追加

---
+++ InteractiveViewerAndInputExample/Extension/NSObjectProtocolExtension.swift
@@ -0,0 +1,18 @@
+//
+//  NSObjectProtocolExtension.swift
+//  InteractiveViewerAndInputExample
+//
+//  Created by 酒井文也 on 2020/12/28.
+//
+
+import Foundation
+import UIKit
+
+// NSObjectProtocolの拡張
+extension NSObjectProtocol {
+
+    // クラス名を返す変数"className"を返す
+    static var className: String {
+        return String(describing: self)
+    }
+}
